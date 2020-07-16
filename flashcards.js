a6b0bb298365a6ed056fab5ea69e1932ca58fea0
ShaneMarusczak <marusczaks@gmail.com>
2020-07-16 10:09:12 -0400

    next and back buttons

--- flashcards.js
+++ flashcards.js
@@ -1,12 +1,57 @@
 "use strict";
 (() => {
+	let currentCardIndex = 0;
 	const [flipCard] = document.getElementsByClassName("flip-card");
 	const [innerFlipCard] = document.getElementsByClassName("flip-card-inner");
-	flipCard.addEventListener("click", () => {
-		if (innerFlipCard.classList.contains("flip-card-flip")) {
-			innerFlipCard.classList.remove("flip-card-flip");
-		} else {
-			innerFlipCard.classList.add("flip-card-flip");
+	const studyData = [
+		["Numerator", "Top Number Of A Fraction"],
+		["Denomonator", "Bottom Number Of A Fraction"]
+	];
+	const order = [];
+
+	const randomIntFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+	function setCardData() {
+		[document.getElementById("term").innerText] = studyData[order[currentCardIndex]];
+		[, document.getElementById("definition").innerText] = studyData[order[currentCardIndex]];
+	}
+
+	function generateOrder() {
+		if (order.length != studyData.length) {
+			let nextNumber = randomIntFromInterval(0, studyData.length - 1);
+			while (order.includes(nextNumber)) {
+				nextNumber = randomIntFromInterval(0, studyData.length - 1);
+			}
+			order.push(nextNumber);
+			generateOrder();
 		}
-	});
+	}
+
+	function nextCard() {
+		if (currentCardIndex < studyData.length - 1) {
+			currentCardIndex++;
+			setCardData();
+		}
+	}
+
+	function previousCard() {
+		if (currentCardIndex > 0) {
+			currentCardIndex--;
+			setCardData();
+		}
+	}
+
+	(() => {
+		flipCard.addEventListener("click", () => {
+			if (innerFlipCard.classList.contains("flip-card-flip")) {
+				innerFlipCard.classList.remove("flip-card-flip");
+			} else {
+				innerFlipCard.classList.add("flip-card-flip");
+			}
+		});
+		generateOrder();
+		setCardData();
+		document.getElementById("forwardButton").addEventListener("click", nextCard);
+		document.getElementById("backButton").addEventListener("click", previousCard);
+	})();
 })();
